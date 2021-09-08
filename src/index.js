69d4d30a0f9c89bd2853adbb5d17b22d578537b6
alex.silva <alex.silva@ciss.com.br>
2021-09-08 12:56:42 -0300

    Middleware e rota de deposito

--- src/index.js
+++ src/index.js
@@ -6,13 +6,26 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 /**
  * cpf - string
  * name - string
  * id - uuid
  * statement []
  */
-
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const id = uuidv4();
@@ -34,17 +47,27 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+// app.use(verifyIfExistAccountCPF);
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-  if (!customer) {
-    return response
-      .status(400)
-      .json({ error: "Não foi possível encontrar o cliente" });
-  }
-
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 const port = 3333;
